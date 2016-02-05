26e1bcf749a21fc27cb0f1a2758ef6edd00cc74b
David Meza <davidmeza@outlook.com>
2016-02-05 10:18:56 -0500

    fix a couple styles broken on mobile

--- app/scripts/controllers/mapActionsCtrl.js
+++ app/scripts/controllers/mapActionsCtrl.js
@@ -25,8 +25,8 @@
         isOpen: false,
         openDirection: 'up',
         items: [
-          { tooltipVisible: false, name: "Edit Location", direction: "left", icon: "img/icons/person-pin.svg", action: $scope.editLocation },
-          { tooltipVisible: false, name: "Find Me!", direction: "left", icon: "img/icons/my-location.svg", action: $scope.geoLocate },
+          { tooltipVisible: false, name: "Edit Location", direction: "left", icon: "img/icons/person-pin.svg", action: $scope.editLocation, addIconClass: 'person-pin' },
+          { tooltipVisible: false, name: "Find Me!", direction: "left", icon: "img/icons/my-location.svg", action: $scope.geoLocate, addIconClass: 'my-location' },
           // { tooltipVisible: false, name: "Search Job", direction: "left", icon: "img/icons/search-color.svg", action: $scope.geoLocate },
           // { tooltipVisible: false, name: "Toggle Park Grouping", direction: "left", icon: "img/icons/marker-off.svg", action: $scope.toggleParkGrouping }
         ]
